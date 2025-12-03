4a65662e4c516ab0e0030a30cf9f6a49fb9e47b5
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-03 11:07:00 -0500

    Update MenuItem.hx

--- source/ui/menu/story/MenuItem.hx
+++ source/ui/menu/story/MenuItem.hx
@@ -4,7 +4,7 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
-import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxSpriteContainer;
 import flixel.math.FlxMath;
 import flixel.util.FlxColor;
 
@@ -13,7 +13,7 @@ import play.save.Preferences;
 /**
  * A UI object that's displayed in the settings menu,
  */
-class MenuItem extends FlxSpriteGroup
+class MenuItem extends FlxSpriteContainer
 {
 	/**
 	 * The ease type to use when moving the menu item.
